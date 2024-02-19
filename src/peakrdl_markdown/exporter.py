c548be0e7a435d5575b10f577a9b47f309d80926
sscarfi <154418441+sscarfi@users.noreply.github.com>
2024-02-19 16:26:33 +0100

    Update exporter.py

--- src/peakrdl_markdown/exporter.py
+++ src/peakrdl_markdown/exporter.py
@@ -117,7 +117,10 @@ class MarkdownExporter:  # pylint: disable=too-few-public-methods
         The "Offset" is an integer so that it can be formatted in the parent
         node.
         """
-        offset = node.address_offset
+        data_w = node.owning_addrmap.get_property("udp_data_bus_width") if node.owning_addrmap.get_property("udp_data_bus_width") else 8
+        div = int(data_w/8) if node.owning_addrmap.get_property("udp_use_word_addressing") else 1
+        
+        offset = int(node.address_offset/div)
         identifier = node.inst_name
         if node.is_array:
             assert node.array_dimensions is not None
@@ -125,7 +128,7 @@ class MarkdownExporter:  # pylint: disable=too-few-public-methods
         name = self._node_name_sanitized(node)
 
         table_row: "OrderedDict[str, Union[str, int]]" = OrderedDict()
-        table_row["Offset (Hex)"] = offset
+        table_row["Offset (Hex)"] = hex(offset)
         table_row["Offset (Dec)"] = int(offset)
         table_row["Identifier"] = identifier
         table_row["Name"] = name
