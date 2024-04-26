58c77dfa6cd619c437212f952d729da93a591b76
sscarfi <154418441+sscarfi@users.noreply.github.com>
2024-04-26 13:53:49 +0200

    Replace long dash

--- src/peakrdl_markdown/exporter.py
+++ src/peakrdl_markdown/exporter.py
@@ -81,7 +81,7 @@ class MarkdownExporter:  # pylint: disable=too-few-public-methods
         """
         name = node.get_html_name()
         if name is None:
-            name = "â"
+            name = "---"
         else:
             name = name.replace("\n", "")
         return name
@@ -308,7 +308,7 @@ class MarkdownExporter:  # pylint: disable=too-few-public-methods
         if node.get_property("onwrite") is not None:
             access += ", " + node.get_property("onwrite").name
 
-        reset_value: str = node.get_property("reset", default="â")
+        reset_value: str = node.get_property("reset", default="---")
         if isinstance(reset_value, int):
             reset = f"0x{reset_value:X}"
         else:
